79145d526dceb0e219201e687ec10ff57501f8fc
Anish Kasam <anishkasam@gmail.com>
2024-05-23 15:19:17 -0700

    create outline

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -2,13 +2,103 @@
   // Write your JS here, or import other files
 </script>
 
-<main>
-  <h1>Probability for Machine Learning</h1>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-  <p>Designed to be an interactive review of important probability and statistics
-  concepts that are highly relevant to machine learning</p>
+<main>
+  <div class="container">
+    <h1>Probability & Statistics for Machine Learning</h1>
+
+    <p>An interactive review of important probability and statistics
+    concepts that are highly relevant to machine learning.</p>
+
+    <div id="prob-spaces" class="section">
+      <h2 class="section-header">Probability Spaces</h2>
+      <h4 class="subsection-header">Probability Measures</h4>
+      <p></p>
+    </div>
+
+    <div id="prob" class="section">
+      <h2 class="section-header">Conditional Probability & Independence</h2>
+      <h4 class="subsection-header">Conditional Probability</h4>
+      <p>Conditional probability is the probability that an event occurs given that
+      another event occurs.</p>
+      <h4 class="subsection-header">Bayes' Theorem</h4>
+      <h4 class="subsection-header">Independence</h4>
+    </div>
+
+    <div id="rvs" class="section">
+      <h2 class="section-header">Random Variables</h2>
+      <h4 class="subsection-header">Discrete</h4>
+      <h4 class="subsection-header">Continuous</h4>
+    </div>
+
+    <div id="prob-distrs" class="section">
+      <h2 class="section-header">Probability Distribution Functions</h2>
+      <h4 class="subsection-header">Probability Mass Functions (PMFs)</h4>
+      <h4 class="subsection-header">Probability Density Functions (PDFs)</h4>
+      <h4 class="subsection-header">Cumulative Density Functions (CDFs)</h4>
+    </div>
+
+    <div id="exp" class="section">
+      <h2 class="section-header">Expectation</h2>
+      <h4 class="subsection-header">Discrete Random Variables</h4>
+      <h4 class="subsection-header">Continuous Random Variables</h4>
+    </div>
+
+    <div id="exp-var" class="section">
+      <h2 class="section-header">Variance</h2>
+    </div>
+
+    <div id="common-distrs" class="section">
+      <h2 class="section-header">Common Distributions</h2>
+      <h4 class="subsection-header">Binomial</h4>
+      <h4 class="subsection-header">Poisson</h4>
+      <h4 class="subsection-header">Uniform</h4>
+      <h4 class="subsection-header">Gaussian</h4>
+    </div>
+
+    <div id="theorems" class="section">
+      <h2 class="section-header">Important Theorems</h2>
+      <h4 class="subsection-header">Law of Large Numbers</h4>
+      <h4 class="subsection-header">Central Limit Theorem</h4>
+    </div>
+
+    <div id="para-density-est" class="section">
+      <h2 class="section-header">Parametric Density Estimation</h2>
+      <h4 class="subsection-header">Method of Moments (MOM)</h4>
+      <h4 class="subsection-header">Maximum Likelihood Estimation (MLE)</h4>
+    </div>
+
+    <div id="conf-intervals" class="section">
+      <h2 class="section-header">Confidence Intervals</h2>
+    </div>
+
+    <div id="sampling" class="section">
+      <h2 class="section-header">Sampling</h2>
+      <h4 class="subsection-header">Simple</h4>
+      <h4 class="subsection-header">Stratified</h4>
+      <h4 class="subsection-header">Clustered</h4>
+    </div>
+  </div>
 </main>
 
 <style>
-  /* Write your CSS here */
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+
+  .container {
+    align-content: left;
+    width: 80vw;
+    max-width: 800px;
+    horizontal-align: middle;
+    margin-top: 10vh;
+    font-family: Inter;
+  }
+
+  .section {
+    margin-top: 5vh;
+  }
+
+  .section-header {
+    margin-bottom: 2vh;
+  }
 </style>
